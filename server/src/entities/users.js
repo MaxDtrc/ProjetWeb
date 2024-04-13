814160e34725107669c543dbbb685ce9e39e0acb
Maxime Dautriche <maxdautriche@gmail.com>
2024-04-13 15:34:15 +0200

    test bdd api

--- server/src/entities/users.js
+++ server/src/entities/users.js
@@ -8,14 +8,29 @@ class Users {
     this.db = db
   }
 
-  create(login, password, lastname, firstname) {
+  create(username, password, lastname, firstname) {
     return new Promise((resolve, reject) => {
-      let userid = 1; // À remplacer par une requête bd
-      if(false) {
-        //erreur
+      const user = this.db.collection("users").findOne({
+        "username": {$eq: username}
+      })
+
+      if(u) {
+        console.log("L'utilisateur existe déjà")
         reject();
       } else {
-        resolve(userid);
+        //Ajout de l'utilisateur à la DB
+        this.db.collection("users").insertOne({
+          "username": username,
+          "password": password,
+          "lastname": lastname,
+          "firstname": firstname
+        })
+
+        const user = this.db.collection.findOne({
+          "username": {$eq: username}
+        })
+
+        resolve(user._id);
       }
     });
   }
@@ -43,7 +58,7 @@ class Users {
   }
 
   async exists(login) {
-    const c = await client.db("base1").collection("collec1").findOne( login )
+    const c = await db.collection("collec1").findOne( login )
     return new Promise((resolve, reject) => {
       if(c) {
         //erreur
