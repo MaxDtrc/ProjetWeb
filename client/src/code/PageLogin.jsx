ae92c16df3557478d1d34feb9c31463053cd7d4a
Maxime Dautriche <maxdautriche@gmail.com>
2024-05-01 17:52:19 +0200

    amélioration pageLogin

--- client/src/code/PageLogin.jsx
+++ client/src/code/PageLogin.jsx
@@ -7,34 +7,46 @@ import axios from "axios";
 
 function PageLogin(props) {
   const [formulaire, setFormulaire] = useState("login");
+  const [statutLogin, setstatutLogin] = useState(0);
 
   const userId = Cookies.get('userId');
   const isAdmin = Cookies.get('isAdmin');
 
-  axios.get("/api/session").then(res => {
-    if(res.data.id){
-      props.login(res.data.id, res.data.isAdmin);
-    }
-  }).catch(err => {console.log(err)});
+  function loadSession(){
+    axios.get("/api/session").then(res => {
+      if(res.data.id){
+        props.login(res.data.id, res.data.isAdmin);
+        setstatutLogin(0);
+      }else{
+        setstatutLogin(1);
+      }
+    }).catch(err => {console.log(err)});
+  }
 
-  if (formulaire == "login") {
-    return (
-      <div id="login_page">
-        <Login login={props.login} setForm={setFormulaire} />
-      </div>
-    );
-  } else if (formulaire == "signin") {
-    return (
-      <div id="login_page">
-        <SignIn login={props.login} setForm={setFormulaire} />
-      </div>
-    );
-  } else {
-    return (
-      <div id="signin_form">
-        <p>attends frérot</p>
-      </div>
-    );
+  if(statutLogin == 0) loadSession();
+  
+
+  //On affiche le formulaire de connexion
+  if(statutLogin == 1){
+    if (formulaire == "login") {
+      return (
+        <div id="login_page">
+          <Login login={props.login} setForm={setFormulaire} />
+        </div>
+      );
+    } else if (formulaire == "signin") {
+      return (
+        <div id="login_page">
+          <SignIn login={props.login} setForm={setFormulaire} />
+        </div>
+      );
+    } else {
+      return (
+        <div id="signin_form">
+          <p>attends frérot</p>
+        </div>
+      );
+    }
   }
 }
 
