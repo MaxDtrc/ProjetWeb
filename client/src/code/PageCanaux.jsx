137d16f0cd1016434991b5afa5806384cba685d7
Maxime Dautriche <maxdautriche@gmail.com>
2024-05-04 14:16:40 +0200

    Nettoyage pagecanaux + pagefildiscussion

--- client/src/code/PageCanaux.jsx
+++ client/src/code/PageCanaux.jsx
@@ -3,40 +3,47 @@ import NouveauCanal from "./NouveauCanal";
 import ListeCanaux from "./ListeCanaux";
 import axios from "axios";
 
+//Composant gérant l'affichage de la page de sélection des canaux
 function PageCanaux(props) {
-  const [lstCanaux, setLstCanaux] = useState([]);
+  const [lstCanaux, setLstCanaux] = useState([]); //Liste des canaux à afficher
 
+  //Fonction de mise à jour de la liste
   async function update() {
+    console.log("PageCanaux: mise à jour de la liste des canaux ...")
     try {
-      var lst = []
+      var lst = [] //Création de la liste
       const res = await axios.get("/api/canal"); // On récupère la liste des canaux
       for (var i = 0; i < res.data.length; i++) {
-        if(!res.data[i].isPrivate || props.admin){
+        if(!res.data[i].isPrivate || props.admin){ //Si le canal est publique ou qu'on est admin, on l'affiche
           try {
-            const u = await axios.get("api/user/" + res.data[i].id_auteur); //Pour chaque canal on récupère le nom de l'auteur
-            res.data[i].auteur = u.data.username; //On remplace l'id par le nom dans le resultat
+            res.data[i].auteur = (await idToName(res.data[i].id_auteur)); //On remplace l'id par le nom dans le resultat
           } catch (err) {
-            res.data[i].auteur = "<Deleted User>";
+            res.data[i].auteur = "<Deleted User>"; //L'utilisateur n'existe plus
           }
-          res.data[i].titre += (res.data[i].isPrivate) ? " (privé)" : ""
-          lst.push(res.data[i])
+          res.data[i].titre += (res.data[i].isPrivate) ? " (privé)" : "" //On indique si le canal est privé
+          lst.push(res.data[i]) //Ajout du canal à la liste à afficher
         }
       }
-      setLstCanaux(lst); //Mise à jour de la liste
+
+      //Mise à jour de la liste
+      console.log("PageCanaux: mise à jour effectuée !")
+      setLstCanaux(lst); 
     } catch (e) {
-      console.log("Erreur dans l'obtention de la liste des canaux");
+      //Erreur lors de la création de la liste
+      console.log("PageCanaux: erreur lors de la mise à jour de la liste des canaux");
     }
   }
 
-  if (lstCanaux.length == 0) {
-    update(); //On met à jour la liste si elle est vide (à ouverture de la page)
-  }
-
+  //On met à jour la liste si elle est vide
+  if (lstCanaux.length == 0) update(); 
+    
+  //Fonction d'ouverture d'un canal
   function openCanal(id) {
-    props.setIdCanal(id);
-    props.setPage("fil_discussion"); //Fonction de l'ouverture du canal
+    props.setIdCanal(id); //On change l'id du canal ouvert
+    props.setPage("fil_discussion"); //On change la page sur laquelle on est
   }
 
+  //Affichage du composant: formulaire de création d'un canal et liste des canaux
   return (
     <div id="liste_canaux">
       <NouveauCanal userId={props.userId} update={update} isAdmin = {props.admin}/>
--- client/src/code/PageFilDiscussion.jsx
+++ client/src/code/PageFilDiscussion.jsx
@@ -4,63 +4,87 @@ import NouveauMessage from "./NouveauMessage";
 import {idToName} from "./utils.js"
 import axios from "axios";
 
+//Composant permettant d'afficher un fil de discussion
 function PageFilDiscussion(props) {
-  const [lstMessages, setLstMessages] = useState([]);
+  const [lstMessages, setLstMessages] = useState([]); //Liste des messages affichés
+  const [replyId, setReplyId] = useState(""); //Id local du message auquel on répond
+  const [replyAuteur, setReplyAuteur] = useState(""); //Id de l'auteur auquel on répond
+  const [replyMessage, setReplyMessage] = useState(""); //Texte du message auquel on répond
 
-  //Réponses
-  const [replyId, setReplyId] = useState("");
-  const [replyAuteur, setReplyAuteur] = useState("");
-  const [replyMessage, setReplyMessage] = useState("");
-
+  //Fonction de mise à jour de la liste des messages
   async function update() {
-    const canal = await axios.get("/api/canal/" + props.idCanal); // On récupère le canal
-
+    console.log("PageFilDiscussion: mise à jour de la liste des messages ...")
+    var canal;
+    try {
+      canal = await axios.get("/api/canal/" + props.idCanal); // On récupère le canal
+    }
+    catch(e){
+      //Erreur pendant la requête d'obtention au serveur
+      console.log("PageFilDiscussion: erreur lors de l'obtention du canal")
+      return;
+    }
+    
     const lst = canal.data.liste_messages; // On récupère la liste des messages du canal
     for (var i = 0; i < lst.length; i++) {
-      lst[i].id = i;
+      lst[i].id = i; //On ajoute l'id local du message (correspondant à son placement dans la liste)
       lst[i].id_auteur = lst[i].auteur; //On copie l'id de l'auteur
-      lst[i].auteur = await idToName(lst[i].auteur);
-      if(!lst[i].reply_auteur || !lst[i].reply_message){
-        lst[i].reply_auteur = ""; lst[i].reply_message = "";
+      lst[i].auteur = await idToName(lst[i].auteur); //On remplace l'id de l'auteur par son nom
+      if(!lst[i].reply_auteur || !lst[i].reply_message){ 
+        lst[i].reply_auteur = ""; lst[i].reply_message = ""; //Si le message n'est en réponse à aucun, on rempli des champs vides
       }else{
-        lst[i].reply_auteur = await idToName(lst[i].reply_auteur);
+        lst[i].reply_auteur = await idToName(lst[i].reply_auteur); //On remplace l'id de l'auteur du message d'origine par son nom
       }
     }
+
+    //Mise à jour de la liste
+    console.log("PageFilDiscussion: mise à jour effectuée !")
     setLstMessages(lst);
   }
 
+  //Fonction de suppression d'un message
   function deleteMessage(id_message){
+    console.log("PageFilDiscussion: demande de supression du message " + id_message)
     axios.delete("/api/canal/" + props.idCanal + "/messages/" + id_message)
     .then(r => {
-      console.log("message supprimé")
-      update();
+      //Supression du message effectuée
+      console.log("PageFilDiscussion: suppression du message effectuée")
+      update(); //Mise à jour de la liste
     })
     .catch(err => {
-      console.log("erreur dans la suppression")
+      //Erreur
+      console.log("PageFilDiscussion: erreur lors de la suppression du message")
     })
   }
 
+  //Si la liste des messages est vide, on appelle la fonction de mise à jour
   if (lstMessages.length == 0) update();
 
+  //Fonctiond d'ajout de message
   function ajouterMessage(msg) {
+    console.log("PageFilDiscussion: demande d'ajout du message")
     axios
       .put("/api/canal/" + props.idCanal, {
         text: msg,
         id_auteur: props.userId,
-        reply_auteur: replyAuteur, //TODO mettre id et pas nom en dur (car ne change pas même si le pseudo change)
+        reply_auteur: replyAuteur,
         reply_message: replyMessage,
         reply_id: replyId
       })
       .then((res) => {
-        setReplyAuteur("")
-        setReplyMessage("") 
-        update();
+        console.log("PageFilDiscussion: ajout du message effectué")
+        setReplyAuteur("") //On vide l'id de l'auteur auquel on répondait
+        setReplyMessage("") //On vide le contenu du message auquel on répondait
+        setReplyId(-1) //On vide l'id du message auquel on répondait
+        update(); //Mise à jour de la liste
       })
       .catch((err) => {
+        //Erreur
+        console.log("PageFilDiscussion: erreur lors de l'ajout du message")
         console.log(err);
       });
   }
 
+  //Affichage du composant: formulaire de nouveau message + liste des messages
   return (
     <>
       <div id="fil_discussion">
