86d75dbcba44a7ac6e7f265c213013411e112089
Maxime Dautriche <maxdautriche@gmail.com>
2024-05-07 09:42:30 +0200

    Optimisation massive de l'affichage des messages (memoisation)

--- client/src/code/PageFilDiscussion.jsx
+++ client/src/code/PageFilDiscussion.jsx
@@ -23,19 +23,50 @@ function PageFilDiscussion(props) {
       console.log("PageFilDiscussion: erreur lors de l'obtention du canal")
       return;
     }
+
+    //Dictionnaire d'optimisation
+    var donneesChargees = {}
     
     const lst = canal.data.liste_messages; // On récupère la liste des messages du canal
     for (var i = 0; i < lst.length; i++) {
       lst[i].id = i; //On ajoute l'id local du message (correspondant à son placement dans la liste)
       lst[i].id_auteur = lst[i].auteur; //On copie l'id de l'auteur
-      lst[i].auteur = await idToName(lst[i].auteur); //On remplace l'id de l'auteur par son nom
+
+      //On remplace l'id de l'auteur par son nom
+      if(donneesChargees[lst[i].auteur]){
+        //Déjà chargé précedemment, on récupère directement la valeur
+        lst[i].auteur = donneesChargees[lst[i].auteur].nom;
+      }else{
+        //Requête à la bdd pour l'obtenir
+        const nom = await idToName(lst[i].auteur);
+        donneesChargees[lst[i].auteur] = {'nom': nom};
+        lst[i].auteur = nom;
+      }
+
       if(!lst[i].reply_auteur || !lst[i].reply_message){ 
         lst[i].reply_auteur = ""; lst[i].reply_message = ""; //Si le message n'est en réponse à aucun, on rempli des champs vides
       }else{
-        lst[i].reply_auteur = await idToName(lst[i].reply_auteur); //On remplace l'id de l'auteur du message d'origine par son nom
+        //On remplace le nom dans le message d'origine auquel on a répondu
+        if(donneesChargees[lst[i].reply_auteur]){
+          //Déjà chargé précedemment, on récupère directement la valeur
+          lst[i].reply_auteur = donneesChargees[lst[i].reply_auteur].nom;
+        }else{
+          //Requête à la bdd pour l'obtenir
+          const nom = await idToName(lst[i].reply_auteur);
+          donneesChargees[lst[i].reply_auteur] = {'nom': nom};
+          lst[i].reply_auteur = nom;
+        }
       }
-      const photo = await idToPhoto(lst[i].id_auteur)
-      console.log("photo = " + photo)
+
+      //Chargement de la photo
+      var photo;
+      if(donneesChargees[lst[i].id_auteur].photo)
+        photo = donneesChargees[lst[i].id_auteur].photo
+      else{
+        donneesChargees[lst[i].id_auteur].photo = await idToPhoto(lst[i].id_auteur)
+        photo = donneesChargees[lst[i].id_auteur].photo;
+      }
+        
       lst[i].photo = photo;
     }
 
